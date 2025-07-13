1766041863964d46937dcb9b98e8921aeb0eb78c
drumboy02 <153551032+drumboy02@users.noreply.github.com>
2025-07-13 17:57:40 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Improving-the-Caesar-cipher
+
+This script is for the 2.5.6 LAB "Improving the Caesar cipher" from the Cisco
+NetAcad Python Essentials 2 course.
+
+The program prompts the user for a string and a shift key and returns the
+encoded text.
---
+++ improving_caesar_cipher.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+while True:
+  # ask the user for one line of text to encrypt
+  text = input("Enter one line of text to encrypt: ")
+  if len(text) == 0:
+    continue
+  
+  try:
+    # ask the user for a shift value (1 - 25). validate
+    shift = int(input("Enter a shift value 1-25: "))
+    if shift < 1 or shift > 25:
+      print("Shift value must be a number between 1 and 25")
+      continue
+    else:
+      break
+  except ValueError:
+    print("Shift value must be a number between 1 and 25")
+    continue
+
+def caesar(text, shift):
+  enctext = ''
+  
+  for c in text:
+    if not c.isalpha():
+      enctext += c
+    else:
+      code = ord(c) + shift
+      
+      if c.isupper():
+        if code > ord('Z'):
+          code = (code - ord('Z')) + ord('A') - 1
+      elif c.islower():
+        if code > ord('z'):
+          code = (code - ord('z')) + ord('a') - 1
+          
+      enctext += chr(code)
+
+  return enctext
+
+# print out the encoded text
+print(caesar(text, shift))
